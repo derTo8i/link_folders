dc62cbd82436ee1d8ccdcbe215f489b3f7571a18
derTo8i <95176424+derTo8i@users.noreply.github.com>
2024-10-11 23:52:50 +0200

    updated the file to execute the command with sudo from Windows 11 24H2 instead of creating a file and manually execute it with admin privileges

--- link_folders.py
+++ link_folders.py
@@ -1,40 +1,45 @@
-from os import path, system, remove
+from os import path, system
 from tkinter.filedialog import askdirectory
 
 
-def get_dir_path(title:str | None):
-    """
-    Prompts the user to select a directory.
-    
-    @param title (str): The title of the directory selection dialog. Default is "Select a directory".
-    @return path (str): The path of the selected directory.
-    """
-    path = askdirectory(title=title)
-    while not path: 
-        askdirectory(title=title)
-    return path
-
-def create_folder_junction():
+def create_folder_junction() -> int:
     """
     This function creates a junction link (symbolic link) between two folders. 
+    
+    @return -1 (int): 
+    @return -2 (int): 
     """
-    original_folder_path = get_dir_path("Original")
-    parent_folder_path = get_dir_path("Parentpath")
-    junction_folder_name = input("Input foldername of the new folder (symbolic link): ")
+    original_folder_path = "D:/#/"#askdirectory(title="Original")
+    if not original_folder_path: return -1
+    parent_folder_path = "D:/" #askdirectory(title="Parentpath")
+    if not parent_folder_path: return -2
+    junction_folder_name = "a" #input("Input foldername of the new folder (symbolic link): ")
 
+
+    print("Overview: ")
+    print(f"Original {original_folder_path}")
+    print(f"Parent {parent_folder_path}")
+    print(f"Name {junction_folder_name}")
+    
     new_folder_path = path.join(parent_folder_path, junction_folder_name)
-
     while junction_folder_name == "" or path.exists(new_folder_path):
-        junction_folder_name = input("file exists or foldername is not a text. Please input new name: ")
+        junction_folder_name = input(f"Folder {new_folder_path} or foldername is not a text. Please input new name: ")
         new_folder_path = path.join(parent_folder_path, junction_folder_name)
 
     print(original_folder_path)
     print(new_folder_path)
+    
+    command = ("sudo mklink /J " + '"' + new_folder_path.replace("/", "\\") + '" '+ '"' + original_folder_path.replace("/", "\\") + '"')
+    print(f"-{command}-")
+    system(command)
 
-    system("sudo mklink /J " + '"' + new_folder_path + '" '+ '"' + original_folder_path + '" ')
-
+def main():
+    result = create_folder_junction()
+    match result: 
+        case -1:
+            exit
+        case -2: 
+            exit
 
 if __name__ == "__main__":
-    create_folder_junction()
-
-#system("Echo off")
+    main()
